2c90c89424e653eb3456bbce68440ff62264b5d1
samunt <sammy.unterman@gmail.com>
2020-07-16 18:26:07 -0400

    Wip

---
+++ react-context/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from './Navbar';
+import Form from './Form';
+import PageContent from './PageContent'
+import {ThemeProvider} from "./contexts/ThemeContext";
+function App() {
+  return (
+      <ThemeProvider>
+          <PageContent>
+            <Navbar/>
+            <Form/>
+          </PageContent>
+      </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ react-context/src/Navbar.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
+import Switch from '@material-ui/core/Switch';
+import { withStyles } from "@material-ui/core";
+import styles from "./styles/NavBarStyles";
+import {ThemeContext} from "./contexts/ThemeContext";
+
+class Navbar extends Component {
+    static contextType = ThemeContext;
+    render() {
+        const {classes} = this.props;
+        return (
+            <div className={classes.root}>
+                <AppBar position='static' color="primary">
+                    <Toolbar>
+                        <IconButton className={classes.menuButton} color="inherit">
+                            <span>🇫🇷</span>
+                        </IconButton>
+                        <Typography className={classes.title} variant='h6' color='inherit'>
+                            App title
+                        </Typography>
+                        <Switch/>
+                        <div className={classes.grow}/>
+                        <div className={classes.search}>
+                            <div className={classes.searchIcon}>
+                                <SearchIcon/>
+                            </div>
+                            <InputBase placeholder='Search...' classes={{
+                                root: classes.inputRoot,
+                                input: classes.inputInput
+                            }}/>
+                        </div>
+                    </Toolbar>
+                </AppBar>
+            </div>
+        )
+    }
+}
+
+export default withStyles(styles)(Navbar)
